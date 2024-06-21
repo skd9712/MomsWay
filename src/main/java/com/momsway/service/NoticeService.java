da22ebccaa362ce320168414ae826538ba680027
loveyrooney <loveyrooney@gmail.com>
2024-06-21 14:08:51 +0900

    [feat] notice findList, findTopList

--- src/main/java/com/momsway/service/NoticeService.java
+++ src/main/java/com/momsway/service/NoticeService.java
@@ -1,4 +1,14 @@
 package com.momsway.service;
 
+import com.momsway.dto.NoticeDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface NoticeService {
+    Page<NoticeDTO> findList(Pageable pageable);
+    long listCount();
+
+    List<NoticeDTO> findTopList();
 }
