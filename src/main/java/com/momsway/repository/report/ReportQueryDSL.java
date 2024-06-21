44b8bd392c347200a7b6f57e0abe55c0e8b2d182
hong <yunahong815@gmail.com>
2024-06-21 15:34:40 +0900

    [feat] reportlist구현

--- src/main/java/com/momsway/repository/report/ReportQueryDSL.java
+++ src/main/java/com/momsway/repository/report/ReportQueryDSL.java
@@ -1,10 +1,12 @@
 package com.momsway.repository.report;
 
 import com.momsway.domain.Report;
-import com.momsway.dto.ReportDTO;
 
 import java.util.List;
 
 public interface ReportQueryDSL {
     List<Report> findByEid(Long eid);
+
+
+    List<Report> findAllReport();
 }
--- src/main/java/com/momsway/service/ReportService.java
+++ src/main/java/com/momsway/service/ReportService.java
@@ -1,7 +1,17 @@
 package com.momsway.service;
 
+import com.momsway.domain.Report;
 import com.momsway.dto.ReportDTO;
+
+import java.util.List;
 
 public interface ReportService {
     int delReport(ReportDTO dto);
+
+   List<ReportDTO> findAllReport();
+
+
+
+
+
 }
