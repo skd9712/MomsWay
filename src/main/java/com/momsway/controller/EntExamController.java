661ad8c1d43557baaee35b1735ba7d7790485219
skd9712 <skd97122@gmail.com>
2024-06-21 13:18:24 +0900

    입시게시판 페이징완료

--- src/main/java/com/momsway/controller/EntExamController.java
+++ src/main/java/com/momsway/controller/EntExamController.java
@@ -1,18 +1,44 @@
 package com.momsway.controller;
 
+import com.momsway.dto.EntExamDTO;
 import com.momsway.dto.LikeDTO;
+import com.momsway.service.EntExamService;
 import com.momsway.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class EntExamController {
     private final LikeService likeService;
+    private final EntExamService entExamService;
+    @GetMapping("entexam")
+    public String entexam(@PageableDefault(size = 10,page = 0) Pageable pageable, Model model){
+        Page<EntExamDTO> entList = entExamService.entlist(pageable);
+        int pagesize = 5;
+        int startPage =((int)(Math.ceil(pageable.getPageNumber()/pagesize)))*pagesize+1;
+        int entPage =Math.min(startPage+pagesize-1,entList.getTotalPages());
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",entPage);
+//        List<EntExamDTO> entList = entExamService.entlist();
+        model.addAttribute("entList",entList);
+        return "entexam/entexam";
+    }
+    @GetMapping("insertent")
+    public String insertEnt(){
+        return "entexam/insertent";
+
+    }
     /*
         list (html 리턴)
         fetch용 list
--- src/main/java/com/momsway/repository/entexam/EntExamQueryDSLImpl.java
+++ src/main/java/com/momsway/repository/entexam/EntExamQueryDSLImpl.java
@@ -1,7 +1,57 @@
 package com.momsway.repository.entexam;
 
+import com.momsway.domain.EntExam;
+import com.momsway.domain.QEntExam;
+import com.momsway.dto.EntExamDTO;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import static com.momsway.domain.QEntExam.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
+@RequiredArgsConstructor
 public class EntExamQueryDSLImpl implements EntExamQueryDSL {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<EntExamDTO> orderlist(Pageable pageable) {
+        BooleanBuilder builder = new BooleanBuilder();
+        List<EntExamDTO> list = queryFactory.select(Projections.fields(EntExamDTO.class, entExam.eid, entExam.title, entExam.createAt, entExam.readNo))
+                .from(entExam)
+                .orderBy(entExam.eid.desc())
+                .where(builder)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        Long totalCount = queryFactory.select(entExam.count())
+                .from(entExam)
+                .where(builder)
+                .fetchOne();
+        return new PageImpl<>(list,pageable,totalCount);
+    }
+
+
+//    @Override
+//    public List<EntExam> orderlist() {
+//        List<Tuple> fetch
+//                = queryFactory.select(entExam.eid, entExam.title, entExam.createAt, entExam.readNo).from(entExam).orderBy(entExam.eid.desc()).fetch();
+//
+//        return fetch.stream().map(tuple -> {
+//            EntExam entExam = new EntExam();
+//            entExam.setEid(tuple.get(QEntExam.entExam.eid));
+//            entExam.setTitle(tuple.get(QEntExam.entExam.title));
+//            entExam.setCreateAt(tuple.get(QEntExam.entExam.createAt));
+//            entExam.setReadNo(tuple.get(QEntExam.entExam.readNo));
+//            return entExam;
+//        }).collect(Collectors.toList());
+//    }
 }
