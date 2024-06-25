92a0272db974acdaf6b12d0ca0c12219a20dec98
skd9712 <skd97122@gmail.com>
2024-06-25 20:54:41 +0900

    흑흑

--- src/main/resources/static/js/entexam/entlike.js
+++ src/main/resources/static/js/entexam/entlike.js
@@ -4,10 +4,17 @@ window.entlike.init = function (eid){
     console.log('like eid: ',eid);
     checkLikeStatus(eid);
 }
-function likeEnt(){
+async function toggleLike(likeBtn, eid) {
+    if (liked) {
+        await unlikeEnt(eid);
+    } else {
+        await likeEnt(eid);
+    }
+}
+async function likeEnt(){
     const token = document.querySelector("meta[name='_csrf']").content;
     const header = document.querySelector("meta[name='_csrf_header']").content;
-    fetch('/insertlike', {
+    await fetch('/insertlike', {
         method: 'POST',
         headers: {
             'X-Requested-With': "XMLHttpRequest",
@@ -22,24 +29,26 @@ function likeEnt(){
         else
             return response.json();
     }).then(data=>{
-        updateLikeButton(data.liked);
-        liked = true
+        liked = data.liked;
+        updateLikeButton(liked);
+        alert('게시글을 좋아합니다')
+        location.href="/entdetail/"+eid;
     }).catch(error=>{
         console.log(error);
     })
 }
-function checkLikeStatus() {
+async function checkLikeStatus() {
     const token = document.querySelector("meta[name='_csrf']").content;
     const header = document.querySelector("meta[name='_csrf_header']").content;
     let query="uid="+1+"&eid="+eid;
-    let liked = false;
+    liked = false;
+    console.log('초기값',liked);
 
-    fetch('/checklike?'+query, {
+    await fetch('/checklike?'+query, {
         headers: {
             'X-Requested-With': "XMLHttpRequest",
             'Accept': 'application/json',
             'X-XSRF-Token': token,
-            //  'Content-Type': 'application/json'
             'Content-Type': 'application/x-www-form-urlencoded'
         }
     }).then(response => {
@@ -49,15 +58,16 @@ function checkLikeStatus() {
             return response.json();
     }).then(data => {
         // 초기 좋아요 상태 업데이트
-        liked = true;
-        updateLikeButton(data.liked);
-        console.log("liked: ", data.liked);
+        liked = data;
+        console.log(data);
+        updateLikeButton(liked);
+        console.log("liked: ", liked);
 
     }).catch(error => {
         console.log(error);
     })
 }
-function updateLikeButton(liked) {
+async function updateLikeButton(liked) {
     const likeBtn = document.getElementById('likeBtn');
     if (liked === true) {
         likeBtn.src = '/images/like_filled.png';
