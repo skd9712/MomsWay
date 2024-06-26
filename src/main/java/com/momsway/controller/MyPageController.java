9f10844dbbc054fb7ff7163255f0a83e981ab0ca
skd9712 <skd97122@gmail.com>
2024-06-26 19:55:11 +0900

    회원정보 수정

--- src/main/java/com/momsway/controller/MyPageController.java
+++ src/main/java/com/momsway/controller/MyPageController.java
@@ -1,15 +1,15 @@
 package com.momsway.controller;
 
 import com.momsway.dto.EntExamDTO;
+import com.momsway.dto.UserDTO;
 import com.momsway.service.EntExamService;
 import com.momsway.service.LikeService;
 import com.momsway.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -28,5 +28,22 @@ public class MyPageController {
         List<EntExamDTO> likeTitles = userService.findByUid(username);
         model.addAttribute("likeTitles",likeTitles);
         return "user/mypage";
+    }
+    @GetMapping("/myinfo")
+    public String myinfo(Model model, Principal principal){
+        String username = principal.getName();
+//        long uidByEmail = userService.findUidByEmail(username);
+        UserDTO dto = userService.findUserInfo(username);
+        model.addAttribute("dto",dto);
+        return "user/myinfo";
+    }
+    @PostMapping("/myinfo")
+    public String myinfoupdate(Long uid,@ModelAttribute("dto") UserDTO dto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "user/myinfo";
+        }else {
+            long id = userService.userUpdate(uid,dto);
+            return "redirect:/main";
+        }
     }
 }
--- src/main/java/com/momsway/repository/user/UserQueryDSL.java
+++ src/main/java/com/momsway/repository/user/UserQueryDSL.java
@@ -1,9 +1,12 @@
 package com.momsway.repository.user;
 
 import com.momsway.dto.EntExamDTO;
+import com.momsway.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserQueryDSL {
     List<EntExamDTO> myentlist(long uidByEmail);
+
+    UserDTO findByUserInfo(long uidByEmail);
 }
