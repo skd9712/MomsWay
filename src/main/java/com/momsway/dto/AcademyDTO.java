6ece7d5e6f97731397c36b53a646cb4b3081be09
loveyrooney <loveyrooney@gmail.com>
2024-06-27 16:11:15 +0900

    [feat] academy/notice 게시판 유효성 처리 및 academy 삭제 수정

--- src/main/java/com/momsway/dto/AcademyDTO.java
+++ src/main/java/com/momsway/dto/AcademyDTO.java
@@ -1,6 +1,11 @@
 package com.momsway.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.modelmapper.internal.bytebuddy.implementation.bind.annotation.Empty;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
@@ -12,10 +17,12 @@ import java.util.List;
 @Builder
 public class AcademyDTO {
     private Long aid;
+    //@Size(min =3, message = "nullable = false")
     private String title;
     private String content;
     private Long readNo;
     private LocalDateTime createAt;
+   // @Size(max=5,message = "최대 5개의 이미지만 등록할 수 있습니다.")
     private List<MultipartFile> files;
     private List<String> imgPaths;
     private String imgPath;
