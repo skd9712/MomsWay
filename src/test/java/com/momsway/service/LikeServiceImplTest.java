083ebb7eb12d9eb95d52d1557c8055a72c49e02b
skd9712 <skd97122@gmail.com>
2024-06-28 11:26:29 +0900

    새드 앤딩

---
+++ src/test/java/com/momsway/service/LikeServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.momsway.service;
+
+import com.momsway.repository.like.LikeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeServiceImplTest {
+
+    @Autowired
+    private final LikeRepository likeRepository;
+
+    LikeServiceImplTest(LikeRepository likeRepository) {
+        this.likeRepository = likeRepository;
+    }
+
+    @Test
+    public void t1(){
+        likeRepository.findByUid(1L);
+    }
+}
